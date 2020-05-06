90353fdfd276bc100656bbdc57efbea0f5b87226
Abhijeet Prasad <aprasad@sentry.io>
2020-05-06 15:58:38 -0400

    get rid of flask plugin

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,3 @@
-# Based on https://github.com/open-telemetry/opentelemetry-python/blob/master/docs/getting-started.rst
-from opentelemetry.ext.flask import FlaskInstrumentor
-FlaskInstrumentor().instrument()  # This needs to be executed before importing Flask
-
 import flask
 import requests
 import os
@@ -9,27 +5,24 @@ import os
 import opentelemetry.ext.http_requests
 from opentelemetry import trace
 
-from opentelemetry.ext.flask import instrument_app
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.ext.otcollector.trace_exporter import CollectorSpanExporter
 
-trace.set_tracer_provider(TracerProvider())
-
-exporter = CollectorSpanExporter(
+span_exporter = CollectorSpanExporter(
     service_name="basic-service", endpoint="localhost:55678"
 )
 
-span_processor = BatchExportSpanProcessor(exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
+tracer_provider = TracerProvider()
+trace.set_tracer_provider(tracer_provider)
+span_processor = BatchExportSpanProcessor(span_exporter)
+tracer_provider.add_span_processor(span_processor)
 
 app = flask.Flask(__name__)
-instrument_app(app)
-opentelemetry.ext.http_requests.RequestsInstrumentor().instrument()
 
 @app.route("/")
 def hello():
-    tracer = trace.get_tracer(__name__)
+    tracer = trace.get_tracer_provider().get_tracer(__name__)
     with tracer.start_as_current_span('foo'):
         with tracer.start_as_current_span('bar'):
             with tracer.start_as_current_span('baz'):
