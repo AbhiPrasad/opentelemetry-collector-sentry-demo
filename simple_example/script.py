4656cf1859947356463e71528b3b807dd8f8375d
Abhijeet Prasad <aprasad@sentry.io>
2020-07-15 13:51:13 -0400

    Update config and scripts

--- simple_example/script.py
+++ simple_example/script.py
@@ -1,24 +1,22 @@
 import os
 import time
 
-from opentelemetry import trace
-from opentelemetry.ext.otcollector.trace_exporter import CollectorSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry import trace as otel_trace
+from opentelemetry.ext.otcollector.trace_exporter import CollectorSpanExporter as otel_CollectorSpanExporter
+from opentelemetry.sdk.trace import TracerProvider as otel_TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor as otel_BatchExportSpanProcessor
 
-exporter = CollectorSpanExporter(
-    service_name="basic-service", endpoint="localhost:55678"
-)
+otel_exporter = otel_CollectorSpanExporter(
+    service_name="basic-service", endpoint="localhost:55678")
+otel_trace.set_tracer_provider(otel_TracerProvider())
+otel_tracer = otel_trace.get_tracer(__name__)
+otel_span_processor = otel_BatchExportSpanProcessor(otel_exporter)
+otel_trace.get_tracer_provider().add_span_processor(otel_span_processor)
 
-trace.set_tracer_provider(TracerProvider())
-tracer = trace.get_tracer(__name__)
-span_processor = BatchExportSpanProcessor(exporter)
-
-trace.get_tracer_provider().add_span_processor(span_processor)
-with tracer.start_as_current_span("example-otel-trace"):
-    with tracer.start_as_current_span("bar"):
+with otel_tracer.start_as_current_span("another-sample-trace"):
+    with otel_tracer.start_as_current_span("bar"):
         time.sleep(2.4)
-        with tracer.start_as_current_span("baz"):
+        with otel_tracer.start_as_current_span("baz"):
             time.sleep(0.5)
-        with tracer.start_as_current_span("kaps"):
+        with otel_tracer.start_as_current_span("kaps"):
             time.sleep(1)
